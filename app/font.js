e132552d32cf5342c29460dac42b66ae1d8e2a4b
KevinFRX <xerokf13@gmail.com>
2023-06-20 18:24:36 -0300

    font file indentation fixed

--- app/font.js
+++ app/font.js
@@ -1,4 +1,4 @@
-import { Space_Grotesk as spaceGrotesk} from '@next/font/google'
+import { Space_Grotesk as spaceGrotesk } from '@next/font/google'
 
 export const font = spaceGrotesk({
   weight: ['400', '700'],
